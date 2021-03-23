775568edc4ec423fb2c8a19480f493c0880107ed
Robin COLLAS <rcollas@e3r4p22.42.fr>
2021-03-23 08:38:30 +0100

    akdsfjkasdf

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rcollas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 12:20:49 by rcollas           #+#    #+#             */
-/*   Updated: 2021/03/22 12:11:19 by rcollas          ###   ########.fr       */
+/*   Updated: 2021/03/23 08:32:20 by rcollas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@ int		ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 
 	i = 0;
+	if (n == 0)
+		return (0);
 	if (s1[i] == '\0' || s2[i] == '\0')
 		return (s1[i] - s2[i]);
 	while ((s1[i] && s2[i]) && i < n)
--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: rcollas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 14:07:43 by rcollas           #+#    #+#             */
-/*   Updated: 2021/03/22 12:36:14 by rcollas          ###   ########.fr       */
+/*   Updated: 2021/03/23 08:31:47 by rcollas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@ char	*ft_strstr(char *str, char *to_find)
 		return (str);
 	while (str[i])
 	{
-		while (str[i] == to_find[j])
+		while (str[i + j] == to_find[j])
 		{
 			if (to_find[j + 1] == '\0')
-				return (&str[j + 2]);
-			i++;
+				return (&str[i]);
 			j++;
 		}
+		j = 0;
 		i++;
 	}
 	return (0);
