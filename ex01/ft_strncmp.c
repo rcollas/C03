623456a67fbd3832c77fba1ed5baa41271db05bf
Robin COLLAS <rcollas@e3r4p22.42.fr>
2021-03-23 14:40:10 +0100

    je n'en peux plus

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rcollas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 12:20:49 by rcollas           #+#    #+#             */
-/*   Updated: 2021/03/23 08:32:20 by rcollas          ###   ########.fr       */
+/*   Updated: 2021/03/23 14:38:00 by rcollas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,7 @@ int		ft_strncmp(char *s1, char *s2, unsigned int n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	if (s1[i] == '\0' || s2[i] == '\0')
-		return (s1[i] - s2[i]);
-	while ((s1[i] && s2[i]) && i < n)
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+	while (s1[i] != '\0' && s1[i] == s2[i] && i < n)
 		i++;
-	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
