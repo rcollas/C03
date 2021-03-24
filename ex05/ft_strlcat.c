4c8095d02b0f269d427092b78fcb5e12670a8161
Robin COLLAS <rcollas@e3r4p22.42.fr>
2021-03-24 09:17:07 +0100

    ex05

---
+++ ex05/ft_strlcat.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcollas <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/24 08:40:06 by rcollas           #+#    #+#             */
+/*   Updated: 2021/03/24 09:14:03 by rcollas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	dest_len;
+	unsigned int	src_len;
+
+	i = 0;
+	j = 0;
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (size == 0)
+		return (dest_len + src_len);
+	while (dest[i])
+		i++;
+	while (src[j] && i < size - 1)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	if (j < size)
+		dest[i] = '\0';
+	if (size < dest_len)
+		return (src_len + size);
+	return (dest_len + src_len);
+}
